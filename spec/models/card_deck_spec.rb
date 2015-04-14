544de96ac1082b70f18db7fb166f55dc09ca6d93
Yann Marquet <ymarquet@gmail.com>
2015-04-14 18:13:38 +0100

    start CardDeck#shuffle implementation

--- spec/models/card_deck_spec.rb
+++ spec/models/card_deck_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 RSpec.describe CardDeck do
   subject { described_class.new }
+
   describe '#cards' do
     it 'return an array of card' do
       subject.cards.each do |card|
         expect(card).to be_instance_of(Card)
       end
+    end
+  end
+
+  describe '#shuffle' do
+    it 'shuffles the cards' do
+      expect(subject.cards).to receive(:shuffle)
+      subject.shuffle
     end
   end
 end
